808f86388b5358150e9bd136bf7fe8479898e334
mccrocjr <mccrocjr@rose-hulman.edu>
2017-11-01 14:35:04 -0400

    updated libs with stop

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -93,6 +93,10 @@ class Snatch3r(object):
         self.right_motor.run_forever(speed_sp=-right_speed_entry)
         self.left_motor.run_forever(speed_sp=left_speed_entry)
 
+    def stop(self):
+        self.left_motor.stop(stop_action='brake')
+        self.right_motor.stop(stop_aciton='brake')
+
     def shutdown(self):
         self.running = False
         self.left_motor.stop(stop_action='brake')
