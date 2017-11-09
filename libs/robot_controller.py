3e7e683ef5f06c84ccd3a833c1ccbab09bf15934
hancocmp <hancocmp@rose-hulman.edu>
2017-11-09 13:21:23 -0500

    removed the pac man method for the robot controller

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -35,7 +35,6 @@ class Snatch3r(object):
         assert self.ir_sensor
         assert self.pixy
         self.running = True
-        self.pac_running = True
 
     def drive_inches(self, inches_target, speed_deg_per_second):
         distance_degrees = inches_target * 360 / 4
@@ -143,6 +142,4 @@ class Snatch3r(object):
                     self.stop()
                     print('Heading too far off')
 
-    def run_pac_man(self):
-            while self.pac_running:
-                time.sleep(0.1)
+
