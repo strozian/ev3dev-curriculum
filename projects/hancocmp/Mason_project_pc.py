e2886eaad91916a8a9743627d6951d528f103f6b
hancocmp <hancocmp@rose-hulman.edu>
2017-11-09 12:43:46 -0500

    Added turning commands to the button callbacks. Created a PacMan class

--- projects/hancocmp/Mason_project_pc.py
+++ projects/hancocmp/Mason_project_pc.py
@@ -12,12 +12,13 @@ import time
 import mqtt_remote_method_calls as com
 from PIL import Image, ImageTk
 
+
 def main():
     print('------------------------------------------------------------')
     print('Main')
     print('------------------------------------------------------------')
-    x=320
-    y=200
+    print('Use the arrow keys to move the robot, space to stop, and q to quit')
+
     mqtt_client = com.MqttClient()
     mqtt_client.connect_to_ev3()
 
@@ -26,84 +27,96 @@ def main():
 
     main_frame = ttk.Frame(root, padding=20)
     main_frame.grid()
-
-    image_R = Image.open("PacMan_R.gif")
-    image_L = Image.open("PacMan_L.gif")
-    image_U = Image.open("PacMan_U.gif")
-    image_D = Image.open("PacMan_D.gif")
-
-    pac_R = ImageTk.PhotoImage(image_R)
-    pac_L = ImageTk.PhotoImage(image_L)
-    pac_U = ImageTk.PhotoImage(image_U)
-    pac_D = ImageTk.PhotoImage(image_D)
-
+    pac = PacMan()
     canvas = tkinter.Canvas(main_frame, background="lightgray", width=640, height=400)
     canvas.grid(columnspan=2)
-    F=canvas.create_image(x, y, image=pac_U)
-    time.sleep(2)
-
+    pac.sprite = canvas.create_image(pac.x, pac.y, image=pac.U)
 
     y_button = ttk.Checkbutton(main_frame, text='Yellow')
     y_button.grid(column=0, row=1)
-    y_button['command'] = lambda: yellow(y_button,canvas,F)
+    y_button['command'] = lambda: yellow(y_button)
 
     button = ttk.Checkbutton(main_frame, text='Red')
     button.grid(column=1, row=1)
     button['command'] = lambda: red(button)
 
-    root.mainloop()
 
 # Robot drive commands:
-    root.bind('<Up>', lambda event: drive_forward(mqtt_client, 600, 600))
-    root.bind('<Left>', lambda event: drive_left(mqtt_client, 600, 600))
+    root.bind('<Up>', lambda event: drive_forward(mqtt_client, 600, 600, canvas, pac))
+    root.bind('<Left>', lambda event: drive_left(mqtt_client, 600, 600, canvas, pac))
     root.bind('<space>', lambda event: stop(mqtt_client))
-    root.bind('<Right>', lambda event: drive_right(mqtt_client, 600, 600))
-    root.bind('<Down>', lambda event: drive_backward(mqtt_client, 600, 600))
-    root.bind('<u>', lambda event: send_up(mqtt_client))
-    root.bind('<j>', lambda event: send_down(mqtt_client))
+    root.bind('<Right>', lambda event: drive_right(mqtt_client, 600, 600, canvas, pac))
+    root.bind('<Down>', lambda event: drive_backward(mqtt_client, 600, 600, canvas, pac))
+    root.bind('<q>', lambda event: quit_program(mqtt_client, True))
 
-    # Buttons for quit and exit
-    q_button = ttk.Button(main_frame, text="Quit")
-    q_button.grid(row=5, column=2)
-    q_button['command'] = (lambda: quit_program(mqtt_client, False))
-
-    e_button = ttk.Button(main_frame, text="Exit")
-    e_button.grid(row=6, column=2)
-    e_button['command'] = (lambda: quit_program(mqtt_client, True))
+    root.mainloop()
 
 
-def drive_forward(mqtt_client, right_speed, left_speed):
-    mqtt_client.send_message('drive_forward', [right_speed, left_speed])
+def drive_forward(mqtt_client, right_speed, left_speed, canvas, pac):
+    if pac.direction == 'right':
+        mqtt_client.send_message('turn_degrees', [90, 600])
+        mqtt_client.send_message('drive_forward', [right_speed, left_speed])
+    if pac.direction == 'left':
+        mqtt_client.send_message('turn_degrees', [-90, 600])
+        mqtt_client.send_message('drive_forward', [right_speed, left_speed])
+    if pac.direction == 'down':
+        mqtt_client.send_message('turn_degrees', [180, 600])
+        mqtt_client.send_message('drive_forward', [right_speed, left_speed])
+    pac.direction = 'up'
+    canvas.delete(pac.sprite)
+    pac.sprite = canvas.create_image(pac.x, pac.y, image=pac.U)
 
 
-def drive_backward(mqtt_client, right_speed, left_speed):
-    mqtt_client.send_message('drive_backward', [right_speed, left_speed])
+def drive_backward(mqtt_client, right_speed, left_speed, canvas, pac):
+    if pac.direction == 'right':
+        mqtt_client.send_message('turn_degrees', [-90, 600])
+        mqtt_client.send_message('drive_forward', [right_speed, left_speed])
+    if pac.direction == 'left':
+        mqtt_client.send_message('turn_degrees', [90, 600])
+        mqtt_client.send_message('drive_forward', [right_speed, left_speed])
+    if pac.direction == 'up':
+        mqtt_client.send_message('turn_degrees', [180, 600])
+        mqtt_client.send_message('drive_forward', [right_speed, left_speed])
+    pac.direction = 'down'
+    canvas.delete(pac.sprite)
+    pac.sprite = canvas.create_image(pac.x, pac.y, image=pac.D)
 
 
-def drive_left(mqtt_client, right_speed, left_speed):
-    mqtt_client.send_message('drive_left', [right_speed, left_speed])
+def drive_left(mqtt_client, right_speed, left_speed, canvas, pac):
+    if pac.direction == 'right':
+        mqtt_client.send_message('turn_degrees', [180, 600])
+        mqtt_client.send_message('drive_forward', [right_speed, left_speed])
+    if pac.direction == 'up':
+        mqtt_client.send_message('turn_degrees', [90, 600])
+        mqtt_client.send_message('drive_forward', [right_speed, left_speed])
+    if pac.direction == 'down':
+        mqtt_client.send_message('turn_degrees', [-90, 600])
+        mqtt_client.send_message('drive_forward', [right_speed, left_speed])
+    pac.direction = 'left'
+    canvas.delete(pac.sprite)
+    pac.sprite = canvas.create_image(pac.x, pac.y, image=pac.L)
 
 
-def drive_right(mqtt_client, right_speed, left_speed):
-    mqtt_client.send_message('drive_right', [right_speed, left_speed])
+def drive_right(mqtt_client, right_speed, left_speed, canvas, pac):
+    if pac.direction == 'left':
+        mqtt_client.send_message('turn_degrees', [180, 600])
+        mqtt_client.send_message('drive_forward', [right_speed, left_speed])
+    if pac.direction == 'up':
+        mqtt_client.send_message('turn_degrees', [-90, 600])
+        mqtt_client.send_message('drive_forward', [right_speed, left_speed])
+    if pac.direction == 'down':
+        mqtt_client.send_message('turn_degrees', [90, 600])
+        mqtt_client.send_message('drive_forward', [right_speed, left_speed])
+    pac.direction = 'right'
+    canvas.delete(pac.sprite)
+    pac.sprite = canvas.create_image(pac.x, pac.y, image=pac.R)
 
 
 def stop(mqtt_client):
     mqtt_client.send_message('stop')
 
 
-# Arm command callbacks
-def send_up(mqtt_client):
-    print("arm_up")
-    mqtt_client.send_message("arm_up")
-
-
-def send_down(mqtt_client):
-    print("arm_down")
-    mqtt_client.send_message("arm_down")
-
-
-# Quit and Exit button callbacks
+# Quit button callbacks
 def quit_program(mqtt_client, shutdown_ev3):
     if shutdown_ev3:
         print("shutdown")
@@ -112,7 +125,7 @@ def quit_program(mqtt_client, shutdown_ev3):
     exit()
 
 
-def yellow(button,):
+def yellow(button):
     state = button.instate(['selected'])
     print('Yellow', state)
 
@@ -122,6 +135,20 @@ def red(button):
     print('Red', state)
 
 
+class PacMan(object):
+    def __init__(self):
+        image_R = Image.open("PacMan_R.gif")
+        image_L = Image.open("PacMan_L.gif")
+        image_U = Image.open("PacMan_U.gif")
+        image_D = Image.open("PacMan_D.gif")
 
+        self.R = ImageTk.PhotoImage(image_R)
+        self.L = ImageTk.PhotoImage(image_L)
+        self.U = ImageTk.PhotoImage(image_U)
+        self.D = ImageTk.PhotoImage(image_D)
+        self.direction = 'up'
+        self.sprite = ''
+        self.x = 320
+        self.y = 200
 
 main()
