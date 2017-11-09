95e255e936a854c6526c1d70277c598c3ff7ff26
hancocmp <hancocmp@rose-hulman.edu>
2017-11-08 22:17:40 -0500

    Added remote drive methods to final project

--- projects/hancocmp/Mason_project_pc.py
+++ projects/hancocmp/Mason_project_pc.py
@@ -15,7 +15,8 @@ def main():
     print('------------------------------------------------------------')
     print('Main')
     print('------------------------------------------------------------')
-
+    x=320
+    y=200
     mqtt_client = com.MqttClient()
     mqtt_client.connect_to_ev3()
 
@@ -32,7 +33,7 @@ def main():
     # label.grid()
     canvas = tkinter.Canvas(main_frame, background="lightgray", width=640, height=400)
     canvas.grid(columnspan=2)
-    canvas.create_image(300, 200, image=photo)
+    canvas.create_image(x, y, image=photo)
 
     y_button = ttk.Checkbutton(main_frame, text='Yellow')
     y_button.grid(column=0, row=1)
@@ -44,6 +45,64 @@ def main():
 
     root.mainloop()
 
+# Robot drive commands:
+    root.bind('<Up>', lambda event: drive_forward(mqtt_client, 600, 600))
+    root.bind('<Left>', lambda event: drive_left(mqtt_client, 600, 600))
+    root.bind('<space>', lambda event: stop(mqtt_client))
+    root.bind('<Right>', lambda event: drive_right(mqtt_client, 600, 600))
+    root.bind('<Down>', lambda event: drive_backward(mqtt_client, 600, 600))
+    root.bind('<u>', lambda event: send_up(mqtt_client))
+    root.bind('<j>', lambda event: send_down(mqtt_client))
+
+    # Buttons for quit and exit
+    q_button = ttk.Button(main_frame, text="Quit")
+    q_button.grid(row=5, column=2)
+    q_button['command'] = (lambda: quit_program(mqtt_client, False))
+
+    e_button = ttk.Button(main_frame, text="Exit")
+    e_button.grid(row=6, column=2)
+    e_button['command'] = (lambda: quit_program(mqtt_client, True))
+
+
+def drive_forward(mqtt_client, right_speed, left_speed):
+    mqtt_client.send_message('drive_forward', [right_speed, left_speed])
+
+
+def drive_backward(mqtt_client, right_speed, left_speed):
+    mqtt_client.send_message('drive_backward', [right_speed, left_speed])
+
+
+def drive_left(mqtt_client, right_speed, left_speed):
+    mqtt_client.send_message('drive_left', [right_speed, left_speed])
+
+
+def drive_right(mqtt_client, right_speed, left_speed):
+    mqtt_client.send_message('drive_right', [right_speed, left_speed])
+
+
+def stop(mqtt_client):
+    mqtt_client.send_message('stop')
+
+
+# Arm command callbacks
+def send_up(mqtt_client):
+    print("arm_up")
+    mqtt_client.send_message("arm_up")
+
+
+def send_down(mqtt_client):
+    print("arm_down")
+    mqtt_client.send_message("arm_down")
+
+
+# Quit and Exit button callbacks
+def quit_program(mqtt_client, shutdown_ev3):
+    if shutdown_ev3:
+        print("shutdown")
+        mqtt_client.send_message("shutdown")
+    mqtt_client.close()
+    exit()
+
 
 def yellow(button):
     state = button.instate(['selected'])
@@ -53,6 +112,8 @@ def yellow(button):
 def red(button):
     state = button.instate(['selected'])
     print('Red', state)
+
+
 
 
 main()
