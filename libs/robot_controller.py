3e92de9383235ac757621619dfaecb3181ada9d3
hancocmp <hancocmp@rose-hulman.edu>
2017-11-14 01:01:19 -0500

    Fixed small error

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -149,4 +149,4 @@ class Snatch3r(object):
         ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
         ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
         ev3.Sound.speak('Game Over').wait()
-        self.pac_running =False
+        self.pac_running = False
