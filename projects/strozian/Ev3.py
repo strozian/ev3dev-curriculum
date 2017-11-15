b03d2b378f07e68ff4667c6f44b1887e34d9327d
strozian <srtrozian@rose-hulman.edu>
2017-11-14 19:56:26 -0500

    Austin Strozier's project for EV.3

--- projects/strozian/Ev3.py
+++ projects/strozian/Ev3.py
@@ -5,66 +5,158 @@ import time
 
 ev3.Sound.speak('Find the color')
 
+
 class MyDelegate(object):
     def __init__(self):
         self.robot = robo.Snatch3r()
-        self.list_of_colors = ['blue', 'red', 'green', 'yellow', 'orange']
+        self.list_of_colors = ['blue', 'red', 'green', 'orange', 'purple']
         self.seeking_color = None
         self.test = False
 
+
     def seek_color(self):
-
-        x = self.robot.pixy.value(1)
-        y = self.robot.pixy.value(2)
         w = self.robot.pixy.value(3)
-        h = self.robot.pixy.value(4)
-        if w > 5:
-            self.robot.stop()
-            ev3.Sound.speak(self.seeking_color).wait()
-            time.sleep(.01)
         while self.test:
+
+            self.robot.drive_left(200, 200)
             if w > 5:
+                self.robot.stop()
+
+            while w > 5:
+                self.robot.stop()
+                ev3.Sound.speak(self.seeking_color).wait()
+                time.sleep(.01)
+                break
+
+            w = self.robot.pixy.value(3)
+            while w > 5:
+                w = self.robot.pixy.value(3)
+                x = self.robot.pixy.value(1)
+                h = self.robot.pixy.value(4)
                 print(w)
-                if x < 150:
-                    self.robot.drive_left(100, 100)
-                if x > 170:
-                    self.robot.drive_right(100, 100)
-                if x > 150:
-                    if self.robot.pixy.value(1) < 170:
+                print(x)
+                print('This is h', h)
+
+                if w > 5:
+
+                    if x < 150:
+                        self.robot.drive_left(100, 100)
+                        time.sleep(.01)
+
+                    if x > 170:
+                        self.robot.drive_right(100, 100)
+                        time.sleep(.01)
+
+                    if x > 150:
+                        if x < 170:
+                            self.robot.stop()
+                            self.robot.drive_forward(400, 400)
+                            time.sleep(.5)
+                            self.robot.stop()
+
+                    if w > 55 and w != 255:
                         self.robot.stop()
+                        time.sleep(.01)
+                        self.robot.arm_up()
+                        ev3.Sound.speak('I found it').wait()
+                        self.test = False
+                        break
 
+    def drive_to_color(self):
+        while self.test:
+            w = self.robot.pixy.value(3)
+            self.robot.drive_left(200, 200)
 
+            if w > 5:
+                self.robot.stop()
+
+            while w > 5:
+                self.robot.stop()
+                ev3.Sound.speak(self.seeking_color).wait()
+                time.sleep(.01)
+                break
+
+            w = self.robot.pixy.value(3)
+            while w > 5:
+                w = self.robot.pixy.value(3)
+                x = self.robot.pixy.value(1)
+                h = self.robot.pixy.value(4)
+                print(w)
+                print(x)
+                print('This is h', h)
+
+                if w > 5:
+
+                    if x < 150:
+                        self.robot.drive_left(100, 100)
+                        time.sleep(.01)
+
+                    if x > 170:
+                        self.robot.drive_right(100, 100)
+                        time.sleep(.01)
+
+                    if x > 150:
+                        if x < 170:
+                            self.robot.stop()
+                            self.robot.drive_forward(400, 400)
+                            time.sleep(.5)
+                            self.robot.stop()
+
+                    if w > 80 and w != 255:
+                        self.robot.stop()
+                        time.sleep(.01)
+                        self.robot.arm_down()
+                        ev3.Sound.speak('Payload delivered')
+                        self.test = False
+                        break
 
     def choose_pixy_mode(self, color_to_get):
-
+        test = False
         for i in range(len(self.list_of_colors)):
             if color_to_get == self.list_of_colors[i]:
                 s = i+1
                 self.robot.pixy.mode = "SIG" + str(s)
                 print(self.robot.pixy.mode)
                 self.seeking_color = str(self.list_of_colors[i])
-                ev3.Sound.speak('SIG').wait()
-                ev3.Sound.speak(str(s)).wait()
+                ev3.Sound.speak(str(self.robot.pixy.mode)).wait()
+                time.sleep(.01)
                 self.test = True
+                test = True
                 self.seek_color()
-
-        if self.test is False:
+        if test is False:
             ev3.Sound.speak("Incorrect Input")
             print('Incorrect input')
 
+    def pixy_mode_for_drive(self, color_to_drive_to):
+        test = False
+        for i in range(len(self.list_of_colors)):
+            if color_to_drive_to == self.list_of_colors[i]:
+                s = i + 1
+                self.robot.pixy.mode = "SIG" + str(s)
+                print(self.robot.pixy.mode)
+                self.seeking_color = str(self.list_of_colors[i])
+                time.sleep(.01)
+                ev3.Sound.speak(str(self.robot.pixy.mode)).wait()
+                time.sleep(.01)
+                self.test = True
+                test = True
+                self.drive_to_color()
 
+        if test is False:
+            ev3.Sound.speak("Incorrect Input")
+            print('Incorrect input')
 
     def drive_right(self, right_speed, left_speed):
         self.robot.drive_right(right_speed, left_speed)
 
     def drive_backward(self, right_speed, left_speed):
-        self.robot.drive_backward(right_speed,left_speed)
+        self.robot.drive_backward(right_speed, left_speed)
 
     def drive_left(self, right_speed, left_speed):
-        self.robot.drive_left(right_speed,left_speed)
+        self.robot.drive_left(right_speed, left_speed)
 
     def drive_forward(self, right_speed, left_speed):
-        self.robot.drive_forward(right_speed,left_speed)
+        self.robot.drive_forward(right_speed, left_speed)
 
     def stop(self):
         self.robot.stop()
@@ -74,13 +166,11 @@ class MyDelegate(object):
         self.test = False
         self.robot.shutdown()
 
-
     def arm_up(self):
         self.robot.arm_up()
 
     def arm_down(self):
         self.robot.arm_down()
-
 
 
 def main():
@@ -89,22 +179,4 @@ def main():
     mqtt_client.connect_to_pc()
     my_delegate.robot.loop_forever()
 
-
-    # while True:
-        # width = robot.pixy.value(3)
-        # x = robot.pixy.value(1)
-        # y = robot.pixy.value(2)
-        # if width > 10:
-        #     ev3.Sound.speak('I found the color').wait()
-        #     robot.drive_forward()
-        # time.sleep(1)
-        # if width > 300:
-        #     break
-
-
-    print("Goodbye!")
-
-
-
 main()
-
--- projects/strozian/Project for Pc.py
+++ projects/strozian/Project for Pc.py
@@ -5,6 +5,8 @@ from tkinter import ttk
 
 import mqtt_remote_method_calls as com
 
+from tkinter import *
+
 
 def main():
     find_and_go_to_color()
@@ -15,64 +17,81 @@ def find_and_go_to_color():
     right_speed = 600
     mqtt_client = com.MqttClient()
     mqtt_client.connect_to_ev3()
+
     root = tkinter.Tk()
     root.title("MQTT Remote")
-    main_frame = ttk.Frame(root, padding=20, relief='raised')
+
+    main_frame = ttk.Frame(root, padding=60, relief='raised')
     main_frame.grid()
-    color_label = ttk.Label(main_frame, text="Color you want")
-    color_label.grid(row=0, column=0)
+
+    other_color_label_entry1 = ttk.Entry(main_frame, width=8)
+    other_color_label_entry1.grid(row=1, column=4)
+    other_color_button = ttk.Button(main_frame, text='Enter Other Color')
+    other_color_button.grid(row=2, column=4)
+    other_color_button['command'] = lambda: drive_to_color(mqtt_client, other_color_label_entry1)
+
     color_label_entry = ttk.Entry(main_frame, width=8)
     color_label_entry.grid(row=1, column=0)
     color_button = ttk.Button(main_frame, text="Enter Color")
     color_button.grid(row=2, column=0)
     color_button['command'] = lambda: seek_color(mqtt_client, color_label_entry)
+
+    label_color = Message(main_frame, text='Choose this entry box to pick up a color', justify=CENTER)
+    label_color.grid(row=0, column=0)
+
+    label_color_other = Message(main_frame, text='Choose this entry box to drive to a \n color', justify=CENTER)
+    label_color_other.grid(row=0, column=4)
+
+    info_label = Message(main_frame, text='Be careful to only enter colors on the Pixy Camera', justify=CENTER)
+    info_label.grid(row=0, column=2)
+
     forward_button = ttk.Button(main_frame, text="Forward")
-    forward_button.grid(row=2, column=1)
+    forward_button.grid(row=2, column=2)
     forward_button['command'] = lambda: drive_forward(mqtt_client, right_speed,
                                                       left_speed)
     root.bind('<Up>',
               lambda event: drive_forward(mqtt_client, right_speed, left_speed))
 
     left_button = ttk.Button(main_frame, text="Left")
-    left_button.grid(row=3, column=0)
+    left_button.grid(row=3, column=1)
 
     left_button['command'] = lambda: drive_left(mqtt_client, right_speed, left_speed)
     root.bind('<Left>',
               lambda event: drive_left(mqtt_client, right_speed, left_speed))
 
     stop_button = ttk.Button(main_frame, text="Stop")
-    stop_button.grid(row=3, column=1)
+    stop_button.grid(row=3, column=2)
     stop_button['command'] = lambda: stop(mqtt_client)
     root.bind('<space>', lambda event: stop(mqtt_client))
 
     right_button = ttk.Button(main_frame, text="Right")
-    right_button.grid(row=3, column=2)
+    right_button.grid(row=3, column=3)
     right_button['command'] = lambda: drive_right(mqtt_client, right_speed, left_speed)
     root.bind('<Right>',
               lambda event: drive_right(mqtt_client, right_speed, left_speed))
 
     back_button = ttk.Button(main_frame, text="Back")
-    back_button.grid(row=4, column=1)
+    back_button.grid(row=4, column=2)
     back_button['command'] = lambda: drive_backward(mqtt_client, right_speed, left_speed)
     root.bind('<Down>',
               lambda event: drive_backward(mqtt_client, right_speed, left_speed))
 
     up_button = ttk.Button(main_frame, text="Grab")
-    up_button.grid(row=5, column=0)
+    up_button.grid(row=5, column=1)
     up_button['command'] = lambda: send_up(mqtt_client)
     root.bind('<q>', lambda event: send_up(mqtt_client))
 
     down_button = ttk.Button(main_frame, text="Lower")
-    down_button.grid(row=6, column=0)
+    down_button.grid(row=6, column=1)
     down_button['command'] = lambda: send_down(mqtt_client)
     root.bind('<w>', lambda event: send_down(mqtt_client))
 
     q_button = ttk.Button(main_frame, text="Quit")
-    q_button.grid(row=5, column=2)
+    q_button.grid(row=5, column=3)
     q_button['command'] = (lambda: quit_program(mqtt_client, False))
 
     e_button = ttk.Button(main_frame, text="Exit")
-    e_button.grid(row=6, column=2)
+    e_button.grid(row=6, column=3)
     e_button['command'] = (lambda: quit_program(mqtt_client, True))
 
     root.mainloop()
@@ -90,12 +109,12 @@ def drive_backward(mqtt_client, right_speed, left_speed):
 
 def drive_left(mqtt_client, right_speed, left_speed):
     print('drive_left')
-    mqtt_client.send_message('drive_left', [right_speed, left_speed])
+    mqtt_client.send_message('drive_left', [right_speed-200, left_speed-200])
 
 
 def drive_right(mqtt_client, right_speed, left_speed):
     print('drive_right')
-    mqtt_client.send_message('drive_right', [right_speed, left_speed])
+    mqtt_client.send_message('drive_right', [right_speed-200, left_speed-200])
 
 
 def stop(mqtt_client):
@@ -120,8 +139,13 @@ def quit_program(mqtt_client, shutdown_ev3):
     mqtt_client.close()
     exit()
 
-def seek_color(mqtt_client,color):
-        print("finding color")
-        mqtt_client.send_message('choose_pixy_mode',[color.get()])
 
+def seek_color(mqtt_client, color):
+        print("finding color")
+        mqtt_client.send_message('choose_pixy_mode', [color.get()])
+
+
+def drive_to_color(mqtt_client, other_color_entry1):
+        print('driving to color')
+        mqtt_client.send_message('pixy_mode_for_drive', [other_color_entry1.get()])
 main()
